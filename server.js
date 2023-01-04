2bd364745cd18f4221a8bfddb94fe560c1a20dfe
jefbar1974 <jefbar@gmail.com>
2023-01-03 22:05:07 -0300

    Aula 01

---
+++ server.js
@@ -0,0 +1,16 @@
+import http from 'http'
+
+http.createServer((req, res)  => {
+
+  const total = 5 
+
+  if (total % 2 === 1) {
+    res.end('é impar!')
+  } else {
+    res.end('é par!')
+  }
+ 
+
+
+
+}).listen('3033')
